d0a91bbca05b6127d410deee9221205e3ae6f7f5
Justin Pagtama <jupagtama@gmail.com>
2025-08-11 18:52:29 -0700

    tested and fixed art card for landscape image

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -12,14 +12,35 @@ import { CommonModule } from '@angular/common';
 
 export class Gallery {
   images = [
-    { url: 'https://res.cloudinary.com/dd3s6lmus/image/upload/v1754698117/riceshower_sujvl6.webp' }
-  ];
+    { url: 'https://res.cloudinary.com/dd3s6lmus/image/upload/v1754698117/riceshower_sujvl6.webp'},
+    { url: 'https://res.cloudinary.com/dd3s6lmus/image/upload/v1754962290/widetest_yzcfsi.webp'}
+  ]
   constructor(private dialog: MatDialog) {}
 
   openImageDialog(imageUrl: string) {
-    this.dialog.open(Card, {
-      data: { imageUrl },
-      panelClass: 'custom-dialog-container'
-    });
+    const img = new Image();
+    img.src = imageUrl;
+
+    img.onload = () => {
+      const aspect = img.width / img.height;
+      const maxHeight = window.innerHeight * 0.9;
+      const maxWidth = window.innerWidth * 0.9;
+
+      let width = maxWidth;
+      let height = width / aspect;
+
+      if (height > maxHeight) {
+        height = maxHeight;
+        width = height * aspect;
+      }
+
+      this.dialog.open(Card, {
+        data: { imageUrl },
+        panelClass: 'art-card',
+        width: `${width}px`,
+        maxWidth: '100vw',
+        maxHeight: '100vh'
+      });
+    };
   }
 }
